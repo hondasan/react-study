cd45abe9c48d3aedb97d806a9de569799b82827e
hondasan <86158971+hondasan@users.noreply.github.com>
2022-05-02 23:45:45 +0900

    JSX記法

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDom from "react-dom";
 
-import App from "./App";
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか</p>
+    </React.Fragment>
+  );
+};
 
-ReactDom.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root")
-);
+ReactDom.render(<App />, document.getElementById("root"));
